03601683c63fc47262b492f249672fc1620d3095
Vasanta Kumar <vasantakmr6@gmail.com>
2023-07-07 07:42:15 +0530

    hostel application day 3

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ var usersTableHeaders = ["Room No", "Name", "Phone no"];
 
 var btnAddNewUser = document.createElement("button");
 btnAddNewUser.textContent = "Add New User";
+btnAddNewUser.addEventListener("click", displayNewUserDialog);
 
 var usersTable = document.createElement("table");
+usersTable.className = "usersTable";
 var usersTablerow = usersTable.createTHead().insertRow();
 
 for(let eachHeader of usersTableHeaders) {
@@ -21,7 +23,6 @@ for(let eachHeader of usersTableHeaders) {
 }
 
 for(let eachUser of usersData) {
-    console.log(eachUser)
     var eachRow = usersTable.insertRow();
     
     var cell1 = eachRow.insertCell();
@@ -32,20 +33,77 @@ for(let eachUser of usersData) {
     cell3.textContent = eachUser.phone;
 }
 
-btnAddNewUser.addEventListener("click", f1);
 
 var mainTab = document.getElementsByClassName("main")[0];
 mainTab.appendChild(btnAddNewUser);
 mainTab.appendChild(usersTable)
 
-/*
-1. create a form in the top next to add new user 
-2. have  
 
+var dialogElement = document.getElementsByClassName("dialog")[0];
 
-*/
+function displayNewUserDialog() {
+    dialogElement.style = "display:block; ";
+    dialogElement.innerHTML = "Add new user<br>";
 
+    var errorPrompt = document.createElement("div");
+    errorPrompt.className = "errorPrompt"
+    errorPrompt.style = "color:red;"
+    var roomNoInput = document.createElement("input");
+    roomNoInput.className = "roomNoInput";
+    roomNoInput.placeholder = "Enter Room No";
+    var nameInput = document.createElement("input");
+    nameInput.className = "nameInput"
+    nameInput.placeholder = "Enter Name";
+    var phoneNoInput = document.createElement("input");
+    phoneNoInput.className = "phoneNoInput"
+    phoneNoInput.placeholder = "Enter Phone No";
 
-function f1() {
-    document.getElementsByClassName("dialog")[0].style = "visibility: visible";
+    var closeBtn = document.createElement("button");
+    closeBtn.textContent = "Close";
+    closeBtn.addEventListener("click", closeDialog);
+    var saveBtn = document.createElement("button");
+    saveBtn.textContent = "Add User";
+
+    saveBtn.addEventListener("click", addNewUser)
+
+    dialogElement.appendChild(errorPrompt);
+    dialogElement.appendChild(roomNoInput);
+    dialogElement.appendChild(nameInput);
+    dialogElement.appendChild(phoneNoInput);
+    dialogElement.appendChild(closeBtn);
+    dialogElement.appendChild(saveBtn);
+
 }
+
+function closeDialog() {
+    dialogElement.style = "display:none;";
+}
+
+function addNewUser() {
+    var roomNoInput = document.getElementsByClassName("roomNoInput")[0];
+    var nameInput = document.getElementsByClassName("nameInput")[0];
+    var phoneNoInput = document.getElementsByClassName("phoneNoInput")[0];
+    var errorPrompt = document.getElementsByClassName("errorPrompt")[0];
+    
+    var usersTable = document.getElementsByClassName("usersTable")[0];
+
+    if(roomNoInput.value != "" && nameInput.value != "" && phoneNoInput.value != "") {
+        errorPrompt.textContent = "";
+        var row = usersTable.insertRow();
+
+        row.insertCell().textContent = roomNoInput.value;
+        row.insertCell().textContent = nameInput.value;
+        row.insertCell().textContent = phoneNoInput.value;
+        usersData.push({name: nameInput.value, roomNo: roomNoInput.value, phoneNo:phoneNoInput.value })
+        console.log(usersData)
+        dialogElement.textContent = "User added Successfully";
+        setTimeout(removeDialog, 5000);
+    } else {
+        errorPrompt.textContent = "Enter all the input fields";
+    }
+}
+
+function removeDialog() {
+    dialogElement.style = "display:nonex"
+}
+
